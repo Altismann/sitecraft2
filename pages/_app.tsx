128ea291aa3cc57196230a3eb08417dd9e5fb823
Altismann <87611738+Altismann@users.noreply.github.com>
2024-11-24 22:43:53 +0000

    iohuh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,18 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
-        <title>sitecraft hela</title>
+        <title>sitecraft</title>
+        <meta name="application-name" content="Next.js" />
+<meta name="author" content="Seb" />
+<link rel="author" href="https://nextjs.org" />
+<meta name="author" content="Josh" />
+<meta name="generator" content="Next.js" />
+<meta name="keywords" content="Next.js,React,JavaScript" />
+<meta name="referrer" content="origin-when-cross-origin" />
+<meta name="color-scheme" content="dark" />
+<meta name="creator" content="Jiachi Liu" />
+<meta name="publisher" content="Sebastian Markbåge" />
+<meta name="format-detection" content="telephone=no, address=no, email=no" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
