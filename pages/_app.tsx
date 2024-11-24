68f758ff437ddd8784780cddb916aa47784c4697
Altismann <87611738+Altismann@users.noreply.github.com>
2024-11-24 22:54:02 +0000

    kdsasdadsaf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,17 +8,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Fragment>
       <Head>
         <title>sitecraft</title>
-        <meta name="application-name" content="Next.js" />
-<meta name="author" content="Seb" />
-<link rel="author" href="https://nextjs.org" />
-<meta name="author" content="Josh" />
+     
+<meta name="description" content="Hej! Jag heter Anders, en produktdesigner och webbutvecklare plaserad i Stockholm. Jag skapar enklare gränssnitt för hobbyister och skapare." />
+<link rel="alternate" hrefLang="sv-SE" href="https://www.sitecraft.se" />
+<link rel="alternate" hrefLang="en-UK" href="https://www.sitecraft.se/en" />
+<link rel="icon" href="/favicon.ico" sizes="any" />
+<meta property="og:image" content="/" />
 <meta name="generator" content="Next.js" />
 <meta name="keywords" content="Next.js,React,JavaScript" />
-<meta name="referrer" content="origin-when-cross-origin" />
-<meta name="color-scheme" content="dark" />
-<meta name="creator" content="Jiachi Liu" />
-<meta name="publisher" content="Sebastian Markbåge" />
-<meta name="format-detection" content="telephone=no, address=no, email=no" />
+
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
